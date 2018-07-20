7dc20711d8f9c65fb774ba9a99355f867dc763e6
Glitched Polygons <glitchedpolygons@protonmail.com>
2018-07-20 13:53:21 +0200

    Initial commit. Let's hope this works :>

---
+++ BashService/Bash.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace GlitchedPolygons.Services.Bash
+{
+    public class Bash : IBash
+    {
+        public void Exec(string cmd)
+        {
+            string escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\""
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+        }
+    }
+}
