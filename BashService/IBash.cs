4d978bee8d6cc38a11b0df2241f0fa61ad21c2d3
Glitched Polygons <glitchedpolygons@protonmail.com>
2018-07-20 13:53:21 +0200

    Initial commit. Let's hope this works :>

---
+++ BashService/IBash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GlitchedPolygons.Services.Bash
+{
+    /// <summary>
+    /// Bash command line service interface for ASP.NET Core apps installed on Linux.
+    /// </summary>
+    interface IBash
+    {
+        /// <summary>
+        /// Executes the passed bash command. Only works on ASP.NET Core apps installed on a Linux OS!!!
+        /// </summary>
+        /// <param name="cmd">The command to execute.</param>
+        void Exec(string cmd);
+    }
+}
